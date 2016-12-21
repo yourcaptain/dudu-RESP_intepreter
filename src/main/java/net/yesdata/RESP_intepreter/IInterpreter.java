896dbc0ef167b39558d979ab10370e262cf6f2b0
yuanaiqing <65186848@qq.com>
2016-12-21 16:59:49 +0800

    1.0.4 submit to oss Signed-off-by: yuanaiqing <65186848@qq.com>

--- src/main/java/net/yesdata/RESP_intepreter/IInterpreter.java
+++ src/main/java/net/yesdata/RESP_intepreter/IInterpreter.java
@@ -2,10 +2,32 @@ package net.yesdata.RESP_intepreter;
 
 import java.util.List;
 
+/**
+ * @author yuanaiqing
+ * 
+ * Interface which defines the RESP string parsing scope
+ *
+ */
 public interface IInterpreter {
+	
+	/**
+	 * @param str single command, just like 'INFO' or 'GET akey'
+	 * @return RESP style formatted string
+	 * @throws Exception parsing exception
+	 */
 	String FormatCommand(String str) throws Exception;
 	
+	/**
+	 * @param str multiple commands, just like list which includes strings of 'INFO' and 'GET akey'
+	 * @return RESP style formatted string
+	 * @throws Exception parsing exception
+	 */
 	String FormatCommand(List<String> str) throws Exception;
 	
+	/**
+	 * @param response string that responsed from redis server
+	 * @return RESP data type nodes
+	 * @throws Exception parsing exception
+	 */
 	List<IRespNode> IntepretResponse(String response) throws Exception;
 }
--- src/main/java/net/yesdata/RESP_intepreter/IRespNode.java
+++ src/main/java/net/yesdata/RESP_intepreter/IRespNode.java
@@ -1,8 +1,30 @@
 package net.yesdata.RESP_intepreter;
 
+/**
+ * @author yuanaiqing
+ * 
+ * Interface which identify RESP data type
+ *
+ */
 public interface IRespNode{
+	
+	/**
+	 * @return this node's RESP type
+	 */
 	RESP_DATA_TYPE getItemType();
+	
+	/**
+	 * print on console
+	 */
 	void print();
+	
+	/**
+	 * @return redis serial protocal format
+	 */
 	String toRespFormatString();
+	
+	/**
+	 * @return human readable format
+	 */
 	String toConsoleFormatString();
 }
